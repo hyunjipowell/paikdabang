5c95115154bf1764cb80739ab66432b1a57f6ec7
hyunjipowell <79792495+hyunjipowell@users.noreply.github.com>
2021-03-09 17:54:19 +0900

    Change recommend part

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 $(function(){
+      //nav hover시 sub 보이게
       const subHeight = $(".head__menu-sub").outerHeight;
       $(".sub-js").height(subHeight);
       $(".nav-item").mouseover(function(){
@@ -9,5 +10,21 @@ $(function(){
             $(".head__menu-sub").stop().slideUp(200);
             $(".sub-js").stop().slideUp(300);
       })
+
+      //추천메뉴 클릭시 색상변경하기
       
+
+
+
+      //family site클릭하면 sub보이게
+      $(".foot__family").click(function(){
+            $(this).find(".foot__family-sub").slideToggle(300);
+            $(this).parent()
+                    .toggleClass("active")
+                    .siblings()
+                    .removeClass("active")
+                    .children(".mapSub")
+                    .slideUp(300);
+      })
+
 })
