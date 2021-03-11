b181e8f547691669b9b27b0457e93dabc0b1f3fe
hyunjipowell <79792495+hyunjipowell@users.noreply.github.com>
2021-03-11 17:51:46 +0900

    Struggle to fix

--- js/style.js
+++ js/style.js
@@ -1,6 +1,6 @@
 $(function(){
       //nav hover시 sub 보이게
-      const subHeight = $(".head__menu-sub").outerHeight;
+      var subHeight = $(".head__menu-sub").outerHeight;
       $(".sub-js").height(subHeight);
       $(".nav-item").mouseover(function(){
             $(".head__menu-sub").stop().slideDown(200);
@@ -11,8 +11,53 @@ $(function(){
             $(".sub-js").stop().slideUp(300);
       })
 
-      //추천메뉴 클릭시 색상변경하기
-      
+      //추천메뉴
+      //클릭하면 다른메뉴 보여지도록
+      $(".recommend__text-sub>li").click(function () {
+        var recomNumber = $(this).index();
+        $(this).addClass("active").siblings("li").removeClass("active");
+        $(".recomend__box>div").eq(recomNumber).css("display", "block").siblings().css("display", "none");
+    })
+
+
+
+    var showMenu = 0;
+  var menuCount = $(".menu>li").length;
+  var copyPhoto = $(".menu>li:first").clone();
+  var copyPhoto1 = $(".menu>li").eq(1).clone();
+
+  $(".menu").append(copyPhoto);
+  $(".menu").append(copyPhoto1);
+
+  var Mcount = $(".menu>li").length;
+  console.log(`복사후 배너개수 : ${Mcount}`);
+
+  $(".recomend__list").width(Mcount * menuCount);
+  $(".menu>li").width(menuCount);
+
+    $(".recomend__box>div").each(function () {
+
+    })
+    function moveMenu(){
+        sNum = $(".menu>li").index();
+        s = sNum-2; 
+        if (showMenu === --sNum) {
+            sNum = 0;
+            $(".recomend__list").css("margin-left", 0);
+          }
+          showMenu++;
+          $(".recomend__list")
+            .stop()
+            .animate(
+              {
+                marginLeft: -showMenu * 30 + "%",
+              },
+              1000
+            );
+    }
+    $(".rightBtn").click(function () {
+        moveMenu();
+      });
 
 
 
